ad898eda78fc8a7f87f19b4d5dd67c7c2c7c8572
Simon Wörpel <simon.woerpel@medienrevolte.de>
2020-03-11 18:47:53 +0100

    Fix migrations

---
+++ aleph/migrate/versions/b189d64b0604_merge_heads.py
@@ -0,0 +1,22 @@
+"""merge
+
+Revision ID: b189d64b0604
+Revises: ('2979a1322381', '814647dcb345')
+Create Date: 2020-03-10 12:08:54.783532
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'b189d64b0604'
+down_revision = ('2979a1322381', '814647dcb345')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
