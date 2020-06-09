afee33512fbfa56886b299433fe29434cad89fae
Simon Wörpel <simon.woerpel@medienrevolte.de>
2020-06-09 12:52:55 +0200

    Make eslint not complain

--- ui/src/components/Timeline/util.js
+++ ui/src/components/Timeline/util.js
@@ -1,11 +1,11 @@
 const getUUID = () => {
-    let dt = new Date().getTime();
-    const uuid = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, c => {
-        const r = (dt + Math.random()*16)%16 | 0;
-        dt = Math.floor(dt/16);
-        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
-    });
-    return uuid;
-}
+  let dt = new Date().getTime();
+  const uuid = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, c => {
+    const r = ((dt + Math.random() * 16) % 16) | 0;
+    dt = Math.floor(dt / 16);
+    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
+  });
+  return uuid;
+};
 
-export { getUUID };
+export {getUUID};
